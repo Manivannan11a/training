560fa5becdf0566d61ff4ac45081bbe6e81ca6b2
Manivannan11a <manivannan11a@gmail.com>
2022-04-05 22:20:35 +0530

    Latest changes

--- src/component/layout/Header.js
+++ src/component/layout/Header.js
@@ -1,19 +1,25 @@
 import { Link } from "react-router-dom";
+import { Layout, Menu } from 'antd';
 
-const Header = () => {
+const { Header } = Layout;
+
+const HeaderComponent = () => {
     return(
-       <ul className="menu">
-           <li className="menu-item">
-               <Link to="/">Home</Link>
-           </li>
-           <li className="menu-item">
-                <Link to="/about">About</Link>
-           </li>
-           <li className="menu-item">
-                <Link to="/contact">Contact Us</Link>
-           </li>
-       </ul>
+        <Header className="header">
+            <div className="logo" />
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+                <Menu.Item key="1">
+                    <Link to="/">Home</Link>
+                </Menu.Item>
+                <Menu.Item key="2">
+                    <Link to="/about">about</Link>
+                </Menu.Item>
+                <Menu.Item key="3">
+                    <Link to="/contact">Contact Us</Link>
+                </Menu.Item>
+            </Menu>
+        </Header>
     )
 }
 
-export default Header;
+export default HeaderComponent;
--- src/component/layout/index.js
+++ src/component/layout/index.js
@@ -1,16 +1,39 @@
-import Header from './Header';
 import Footer from './Footer';
 import './layout.css';
+import HeaderComponent from './Header';
+import SideNavBar from './SideNav';
 
-const Layout = ({children}) => {
-    return(
-        <>
-            <Header />
-                <p>Content</p>
-                {children}
-            <Footer />
-        </>
+import { Layout, Breadcrumb } from 'antd';
+
+const { Content } = Layout;
+
+const LayoutComponent = ({ children }) => {
+    return (
+        <Layout>
+            <HeaderComponent />
+            <Layout>
+                <SideNavBar />
+                <Layout style={{ padding: '0 24px 24px' }}>
+                    <Breadcrumb style={{ margin: '16px 0' }}>
+                        <Breadcrumb.Item>Home</Breadcrumb.Item>
+                        <Breadcrumb.Item>List</Breadcrumb.Item>
+                        <Breadcrumb.Item>App</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <Content
+                        className="site-layout-background"
+                        style={{
+                            padding: 24,
+                            margin: 0,
+                            minHeight: 280,
+                        }}
+                    >
+                        {children}
+                    </Content>
+                    <Footer />
+                </Layout>
+            </Layout>
+        </Layout>
     )
 }
 
-export default Layout;
+export default LayoutComponent;
